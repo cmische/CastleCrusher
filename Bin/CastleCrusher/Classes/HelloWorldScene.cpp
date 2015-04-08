57bcc244957156a83ca87e20cacf51a94a9c35b4
Rachelle Clayton <Spanky@Spankys-MacBook-Pro.local>
2015-04-08 15:08:12 -0500

    Update HelloWorld to Transition to Level1

--- Bin/CastleCrusher/Classes/HelloWorldScene.cpp
+++ Bin/CastleCrusher/Classes/HelloWorldScene.cpp
@@ -114,3 +114,40 @@ bool HelloWorld::init()
 
     return true;
 }
+
+void HelloWorld::transitionToLevel1Scene()
+{
+    auto size = Director::getInstance()->getWinSize();
+    
+    
+    this->tileMap = TMXTiledMap::create("Dungeon_Level1.tmx");
+    this->background = _tileMap->layerNamed("Background");
+    addChild(_tileMap, -1);
+    
+    
+    TMXObjectGroup *objects = _tileMap->getObjectGroup("Objects");
+    CCASSERT(NULL!= objects, "'Objects' object group not found");
+    auto playerShowUpPoint = objects->getObject("SpawnPoint");
+    CCASSERT(!playerShowUpPoint.empty(), "PlayerShowUpPoint object not found");
+    
+    int x = playerShowUpPoint["x"].asInt();
+    int y = playerShowUpPoint["y"].asInt();
+    
+    _player = Sprite::create("SirBrawnleyTemp.png");
+    _player->setPosition(x + _tileMap->getTileSize().width / 2, y + _tileMap->getTileSize().height / 2);
+    _player->setScale(0.5);
+    
+    addChild(_player);
+    setViewPointCenter(_player->getPosition());
+    
+    auto listener = EventListenerKeyboard::create();
+    listener->onKeyPressed = CC_CALLBACK_2(Level_1::onKeyPressed, this);
+    this->_eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);
+
+}
+
+void HelloWorld::toLevel1()
+{
+    auto scene = Level_1::createScene();
+    Director::getInstance()->replaceScene(scene);
+}
